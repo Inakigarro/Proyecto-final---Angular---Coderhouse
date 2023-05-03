b69ca563451a82c5697575c5491035e8a8b21da9
inakigarro <inakigarro@gmail.com>
2023-05-02 22:49:28 -0300

    feat: add creation dtos

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -6,8 +6,21 @@ export interface Alumno {
   phone: string;
 }
 
+export interface CreateAlumno {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
 export interface Profesor {
   id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export interface CreateProfesor {
   firstName: string;
   lastName: string;
   email: string;
@@ -20,8 +33,19 @@ export interface Curso {
   inscripciones: number[];
 }
 
+export interface CreateCurso {
+  displayName: string;
+  profesor: Profesor;
+  inscripciones: number[];
+}
+
 export interface Inscripcion {
   id: number;
   curso: Curso;
   alumno: Alumno;
+}
+
+export interface CreateInscripcion {
+  cursoId: number;
+  alumnoId: number;
 }
